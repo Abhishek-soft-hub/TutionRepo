9b6bd5934267da458c39d9322e012bf433da36b8
Abhishek jadhav <abhipersonal4444@gmail.com>
2025-11-11 12:11:28 +0530

    new class added

---
+++ AmitClasses/src/main/java/com/example/Classes/Exception/StudentException.java
@@ -0,0 +1,29 @@
+package com.example.Classes.Exception;
+
+import org.springframework.http.HttpStatus;
+
+public class StudentException extends RuntimeException {
+
+	
+	private String getMessage;
+	private HttpStatus getHttp;
+	
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return getMessage;
+	}
+	
+	public HttpStatus gethttp() {
+		return getHttp;
+	}
+
+	public StudentException(String getMessage, HttpStatus getHttp) {
+		
+		this.getMessage = getMessage;
+		this.getHttp = getHttp;
+	}
+	
+	
+	
+}
