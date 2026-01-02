e8e5531db0d8278938551beb44ace914361a6286
Abhishek-soft-hub <236396774+Abhishek-soft-hub@users.noreply.github.com>
2026-01-02 17:42:01 +0530

    add dto fields

--- AmitClasses/src/main/java/com/example/Classes/Entity/DTO.java
+++ AmitClasses/src/main/java/com/example/Classes/Entity/DTO.java
@@ -1,5 +1,23 @@
 package com.example.Classes.Entity;
 
 public class DTO {
+	private String name;
+	private String add;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdd() {
+		return add;
+	}
+
+	public void setAdd(String add) {
+		this.add = add;
+	}
 
 }
